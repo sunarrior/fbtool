7c611cc695dec75d0a74adbdc516e94c3696e53b
daiphuoc511 <duongdaiphuoc511@gmail.com>
2022-12-03 15:20:48 +0700

    phuoc dep trai

--- fbtool.py
+++ fbtool.py
@@ -8,6 +8,8 @@ dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
 URL = os.environ.get('URL')
 token = os.environ.get('TOKEN')
+token_info = os.environ.get('TOKEN_INFO')
+token_favorite = os.environ.get('TOKEN_FAVORITE')
 
 # ============= Tool Function =============
 def get_and_save_uid(option, opt_str, value, parser):
@@ -21,6 +23,31 @@ def print_uid(option, opt_str, value, parser):
     uid = os.environ.get('USER_ID')
     print(f"USER_ID is {uid}")
 
+def get_info_user(option, opt_str, value, parser):
+    PARAMS_NAME = {'fields':'name', 'access_token':token_info}
+    PARAMS_BIRTHDAY = {'fields':'birthday', 'access_token':token_info}
+    PARAMS_EMAIL = {'fields':'email', 'access_token':token_info}
+    PARAMS_LOCATION = {'fields':'location', 'access_token':token_info}
+    PARAMS_GENDER = {'fields':'gender', 'access_token':token_info}
+
+    user_name = requests.get(url = URL + '/me', params = PARAMS_NAME).json()
+    birthday = requests.get(url = URL + '/me', params = PARAMS_BIRTHDAY).json()
+    email = requests.get(url = URL + '/me', params = PARAMS_EMAIL).json()
+    location = requests.get(url = URL + '/me', params = PARAMS_LOCATION).json()
+    gender = requests.get(url = URL + '/me', params = PARAMS_GENDER).json()
+
+    print(f"Get username: {user_name['name']}")
+    print(f"Get gender: {gender['gender']}")
+    print(f"Get birthday: {birthday['birthday']}")
+    print(f"Get email: {email['email']}")
+    print(f"Get location: {location['location']['name']}")
+
+def get_favorite_teams(option, opt_str, value, parser):
+    PARAMS_FAVORITE = {'fields':'favorite_teams', 'access_token':token_favorite}
+    user_favorite = requests.get(url = URL + '/me', params = PARAMS_FAVORITE).json()
+    print(f"Get list favorite user:")
+    for i in range(len(user_favorite['favorite_teams'])):
+        print(user_favorite['favorite_teams'][i]['name'])
 
 # ============= Command Line Function =============
 def main():
@@ -29,6 +56,10 @@ def main():
                         help="get and save user id")
     parser.add_option("--showuid", action="callback", callback=print_uid,
                         help="show user id")
+    parser.add_option("--getsuinfo", action="callback", callback=get_info_user,
+                        help="show user information")
+    parser.add_option("--getsufavorite", action="callback", callback=get_favorite_teams,
+                        help="show favorite")
 
     
     (options, args) = parser.parse_args()
