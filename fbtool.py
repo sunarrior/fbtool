2ed4745c039aa55889a53f4329f2aeee9830fb37
nhat <nhatthuynhh621@gmail.com>
2022-12-05 21:35:42 +0700

    update code

--- fbtool.py
+++ fbtool.py
@@ -3,14 +3,15 @@ import os
 import dotenv
 import requests
 import json
+import codecs
 
 # ============= Export Environment =============
 dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
 URL = os.environ.get('URL')
 token = os.environ.get('TOKEN')
-token_info = os.environ.get('TOKEN_INFO')
-token_favorite = os.environ.get('TOKEN_FAVORITE')
+# token_info = os.environ.get('TOKEN_INFO')
+# token_favorite = os.environ.get('TOKEN_FAVORITE')
 
 # ============= Tool Function =============
 def check_authenticate_info():
@@ -35,13 +36,13 @@ def get_post_on_feed():
         print("Feed don't have any post!")
         return;
     lst_post_id = post_id['feed']['data']
+    with codecs.open('./data/new_feed.json', 'w', 'utf-8') as file:
+        json.dump(lst_post_id, file, indent=4)
+        file.close()
     return lst_post_id
 
 def show_post_list(option, opt_str, value, parser):
     lst_post_id = get_post_on_feed()
-    with open('list_post.json', 'w') as file:
-        file.write(json.dumps(lst_post_id))
-        file.close()
     print(lst_post_id)
     
 def get_reaction_count(post_id, type):
@@ -75,10 +76,9 @@ def get_reaction_count(post_id, type):
             reaction_count = 0
         reaction_count_json['reaction_count'][name_of_summary] = reaction_count
     
-    with open('reaction_count.json', 'w') as file:
-        file.write(json.dumps(reaction_count_json))
+    with codecs.open('./data/reaction_count.json', 'w') as file:
+        json.dump(reaction_count_json, file, indent=4)
         file.close()
-
     return reaction_count_json
 
 def summary_reactions_on_one_post(option, opt_str, value, parser):
@@ -108,15 +108,16 @@ def get_albums(option, opt_str, value, parser):
         'quantity':len(data['albums']['data']),
         'info': arr
     }
-    with open("./data/album.json", "w+") as outfile:
+    with codecs.open("./data/album.json", "w+", "utf-8") as outfile:
         json.dump(albums, outfile, indent=4)
+        outfile.close()
     print(f"Albums: {albums}")
     
 def get_total_friends(option, opt_str, value, parser):
     PARAMS = {'fields':'friends', 'access_token':token}
     data = requests.get(url = URL + '/me', params = PARAMS).json()
 
-    dotenv.set_key(dotenv_file, "TOTAL_FRIENDS", str(data['friends']['summary']['total_count']))
+    # dotenv.set_key(dotenv_file, "TOTAL_FRIENDS", str(data['friends']['summary']['total_count']))
     print(f"Get total friends: {str(data['friends']['summary']['total_count'])}")
 
 def get_pages_like(option, opt_str, value, parser):
@@ -130,8 +131,9 @@ def get_pages_like(option, opt_str, value, parser):
         'quantity':len(data['likes']['data']),
         'info': arr
     }
-    with open("./data/pages_liked.json", "w+") as outfile:
+    with codecs.open("./data/pages_liked.json", "w", "utf-8") as outfile:
         json.dump(pages_liked, outfile, indent=4)
+        outfile.close()
     print(f"Pages liked: {pages_liked}")
 
 def get_music_like(option, opt_str, value, parser):
@@ -142,25 +144,38 @@ def get_music_like(option, opt_str, value, parser):
     for music in data['music']['data']:
         arr.append(music['name'])
     musics_liked = {
-        'quantity':len(data['music']['data']),
+        'quantity':str(len(data['music']['data'])),
         'info': arr
     }
-    with open("./data/musics_liked.json", "w+") as outfile:
+    with codecs.open("./data/musics_liked.json", "w+", "utf-8") as outfile:
         json.dump(musics_liked, outfile, indent=4)
+        outfile.close()
     print(f"Musics liked: {musics_liked}")
 
 def get_info_user(option, opt_str, value, parser):
-    PARAMS_NAME = {'fields':'name', 'access_token':token_info}
-    PARAMS_BIRTHDAY = {'fields':'birthday', 'access_token':token_info}
-    PARAMS_EMAIL = {'fields':'email', 'access_token':token_info}
-    PARAMS_LOCATION = {'fields':'location', 'access_token':token_info}
-    PARAMS_GENDER = {'fields':'gender', 'access_token':token_info}
+    PARAMS_NAME = {'fields':'name', 'access_token':token}
+    PARAMS_BIRTHDAY = {'fields':'birthday', 'access_token':token}
+    PARAMS_EMAIL = {'fields':'email', 'access_token':token}
+    PARAMS_LOCATION = {'fields':'location', 'access_token':token}
+    PARAMS_GENDER = {'fields':'gender', 'access_token':token}
 
     user_name = requests.get(url = URL + '/me', params = PARAMS_NAME).json()
     birthday = requests.get(url = URL + '/me', params = PARAMS_BIRTHDAY).json()
     email = requests.get(url = URL + '/me', params = PARAMS_EMAIL).json()
     location = requests.get(url = URL + '/me', params = PARAMS_LOCATION).json()
     gender = requests.get(url = URL + '/me', params = PARAMS_GENDER).json()
+
+    basic_info = {
+        "user_name": user_name['name'],
+        "gender": gender['gender'],
+        "birthday": birthday['birthday'],
+        "email": email['email'],
+        "location": location['location']['name'],
+    }
+
+    with codecs.open('./data/basic_info.json', 'w', 'utf-8') as file:
+        json.dump(basic_info, file, indent=4)
+        file.close()
 
     print(f"Get username: {user_name['name']}")
     print(f"Get gender: {gender['gender']}")
@@ -169,11 +184,20 @@ def get_info_user(option, opt_str, value, parser):
     print(f"Get location: {location['location']['name']}")
 
 def get_favorite_teams(option, opt_str, value, parser):
-    PARAMS_FAVORITE = {'fields':'favorite_teams', 'access_token':token_favorite}
+    PARAMS_FAVORITE = {'fields':'favorite_teams', 'access_token':token}
     user_favorite = requests.get(url = URL + '/me', params = PARAMS_FAVORITE).json()
+    favorite_teams_json = {
+        "quantity": len(user_favorite['favorite_teams']),
+        "team_name": []
+    }
     print(f"Get list favorite user:")
     for i in range(len(user_favorite['favorite_teams'])):
+        favorite_teams_json['team_name'].append(user_favorite['favorite_teams'][i]['name'])
         print(user_favorite['favorite_teams'][i]['name'])
+
+    with codecs.open('./data/favorite_teams.json', 'w', 'utf-8-sig') as file:
+        json.dump(favorite_teams_json, file, indent=4)
+        file.close()
 
 # ============= Command Line Function =============
 def main():
@@ -186,7 +210,7 @@ def main():
                         help="-p [post_id] -t [type_reaction] --get_one_post_react, \n \
                                 show reaction count in one post")
     parser.add_option("-p", help="set post id")
-    parser.add_option("-t", help="set type of reaction [Like, Love, Thankful, Haha, Wow, Sad, Angry] \
+    parser.add_option("-t", help="set type of reaction [Like, Love, Care, Haha, Wow, Sad, Angry] \
                                     can be multiple type (ex: haha,love)"
                                     )                   
     parser.add_option("--albums", action="callback", callback=get_albums,
@@ -199,7 +223,7 @@ def main():
                         help="get music liked")
     parser.add_option("--userinfo", action="callback", callback=get_info_user,
                         help="show user information")
-    parser.add_option("--favorite", action="callback", callback=get_favorite_teams,
+    parser.add_option("--favteams", action="callback", callback=get_favorite_teams,
                         help="show favorite")
 
     (options, args) = parser.parse_args()
